441b5604bb10b92f80db62c240fe802dc89a8346
Hiroaki Sano <hsano@student.42tokyo.jp>
2023-07-02 17:44:36 +0900

    change color

--- srcs/render.c
+++ srcs/render.c
@@ -5,8 +5,8 @@ void	init_color_pair(void)
 {
 	start_color();
 	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(3, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_BLACK);
+	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(4, COLOR_BLUE, COLOR_BLACK);
 	init_pair(5, COLOR_GREEN, COLOR_BLACK);
 	init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
